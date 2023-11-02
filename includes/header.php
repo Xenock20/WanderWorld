aaefe9f4e854cbb8a55e6fc5c8af2d8ccc945145
Xenock20 <juliosaboredo@gmail.com>
2023-11-01 21:37:39 -0300

    1-11

---
+++ includes/header.php
@@ -0,0 +1,30 @@
+<div class="overlay"></div>
+<header>
+    <div class="header-content">
+        <div class="logo">
+            <div class="logo-icon">
+                <i class="fas fa-globe"></i> <!-- Icono de Font Awesome -->
+            </div>
+            <div class="logo-text">
+                <span>WanderWorld</span>
+            </div>
+        </div>
+        <div class="navigation">
+            <ul>
+                <li><a href="index.php"><i class="fas fa-home"></i> Inicio</a></li>
+                <li><a href="profile.php"><i class="fas fa-user"></i> Perfil</a></li>
+                <li><a href="explore.php"><i class="fas fa-compass"></i> Explorar</a></li>
+                <li><a href="#"><i class="fas fa-bell"></i> Notificaciones</a></li>
+                <li><a href="settings.php"><i class="fas fa-cog"></i> Configuración</a></li>
+            </ul>
+        </div>
+        <div class="user-profile">
+            <i class="fas fa-user"></i> <!-- Icono de usuario de Font Awesome -->
+            <span>Nombre de Usuario</span>
+            <a href="../login.php">
+                <i class="fas fa-sign-out-alt"></i> <!-- Icono de cerrar sesión de Font Awesome -->
+            </a>
+        </div>
+    </div>
+</header>
+<script src="https://kit.fontawesome.com/5ebed57939.js" crossorigin="anonymous"></script>
