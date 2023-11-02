ff6307522ca047ae21c9d87fa3dda480de6d893e
Xenock20 <juliosaboredo@gmail.com>
2023-11-01 23:14:32 -0300

    Add feed

--- pages/index.php
+++ pages/index.php
@@ -11,6 +11,123 @@
 <body>
     <?php include './../includes/header.php'; ?>
 
+    <div class="content-wrapper">
+        <div class="cont-feed">
+            <div class="post">
+                <div class="user-info">
+                    <img src="user-avatar.jpg" alt="Nombre de Usuario">
+                    <span>Nombre de Usuario</span>
+                </div>
+                <p class="post-content">
+                    Esto es el contenido de la publicación. Puede incluir texto, imágenes y más.
+                </p>
+                <div class="actions">
+                    <div class="like">
+                        <i class="fas fa-heart"></i> <span>25 Likes</span>
+                    </div>
+                    <div class="comments">
+                        <i class="fas fa-comment"></i> <span>10 Comentarios</span>
+                    </div>
+                </div>
+                <div class="comment-section">
+                    <input type="text" placeholder="Añadir un comentario...">
+                    <button>Publicar</button>
+                </div>
+                <div class="comments">
+                    <div class="comment">
+                        <img src="commenter-avatar.jpg" alt="Nombre del Comentarista">
+                        <span>Nombre del Comentarista:</span>
+                        <p>Este es un comentario en la publicación. Puede incluir texto y más.</p>
+                    </div>
+                    <!-- Puedes agregar más comentarios aquí -->
+                </div>
+            </div>
+            <div class="post">
+                <div class="user-info">
+                    <img src="user-avatar.jpg" alt="Nombre de Usuario">
+                    <span>Nombre de Usuario</span>
+                </div>
+                <p class="post-content">
+                    Esto es el contenido de la publicación. Puede incluir texto, imágenes y más.
+                </p>
+                <div class="actions">
+                    <div class="like">
+                        <i class="fas fa-heart"></i> <span>25 Likes</span>
+                    </div>
+                    <div class="comments">
+                        <i class="fas fa-comment"></i> <span>10 Comentarios</span>
+                    </div>
+                </div>
+                <div class="comment-section">
+                    <input type="text" placeholder="Añadir un comentario...">
+                    <button>Publicar</button>
+                </div>
+                <div class="comments">
+                    <div class="comment">
+                        <img src="commenter-avatar.jpg" alt="Nombre del Comentarista">
+                        <span>Nombre del Comentarista:</span>
+                        <p>Este es un comentario en la publicación. Puede incluir texto y más.</p>
+                    </div>
+                    <!-- Puedes agregar más comentarios aquí -->
+                </div>
+            </div>
+            <div class="post">
+                <div class="user-info">
+                    <img src="user-avatar.jpg" alt="Nombre de Usuario">
+                    <span>Nombre de Usuario</span>
+                </div>
+                <p class="post-content">
+                    Esto es el contenido de la publicación. Puede incluir texto, imágenes y más.
+                </p>
+                <div class="actions">
+                    <div class="like">
+                        <i class="fas fa-heart"></i> <span>25 Likes</span>
+                    </div>
+                    <div class="comments">
+                        <i class="fas fa-comment"></i> <span>10 Comentarios</span>
+                    </div>
+                </div>
+                <div class="comment-section">
+                    <input type="text" placeholder="Añadir un comentario...">
+                    <button>Publicar</button>
+                </div>
+                <div class="comments">
+                    <div class="comment">
+                        <img src="commenter-avatar.jpg" alt="Nombre del Comentarista">
+                        <span>Nombre del Comentarista:</span>
+                        <p>Este es un comentario en la publicación. Puede incluir texto y más.</p>
+                    </div>
+                    <!-- Puedes agregar más comentarios aquí -->
+                </div>
+            </div>
+        </div>
+        <div class="suggested-container">
+            <h2>Sugerencias</h2>
+            <div class="suggested-person">
+                <img src="persona1.jpg" alt="Persona 1">
+                <div class="suggested-info">
+                    <p>Nombre de Persona 1</p>
+                </div>
+                <button class="follow-button">Seguir</button>
+            </div>
+            <div class="suggested-person">
+                <img src="persona1.jpg" alt="Persona 1">
+                <div class="suggested-info">
+                    <p>Nombre de Persona 1</p>
+                </div>
+                <button class="follow-button">Seguir</button>
+            </div>
+            <div class="suggested-person">
+                <img src="persona1.jpg" alt="Persona 1">
+                <div class="suggested-info">
+                    <p>Nombre de Persona 1</p>
+                </div>
+                <button class="follow-button">Seguir</button>
+            </div>
+
+            <!-- Otros elementos sugeridos -->
+        </div>
+    </div>
 </body>
 
 </html>
