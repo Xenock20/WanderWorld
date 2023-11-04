96cbf5c9b7c83a154ce3b1ab9e42dd96e7e9e05d
Alejandro <imalegomez2004@gmail.com>
2023-11-04 00:02:54 -0300

    added register

--- signup.php
+++ signup.php
@@ -1,3 +1,20 @@
+<?php
+    include("cn.php");
+
+    $user = $_POST['username'];
+    $email = $_POST['email'];
+    $password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+    $nation = $_POST['nationality'];
+    $sql = "INSERT INTO users (username, email, password_hash, nationality,date_joined) VALUES ('$user', '$email', '$password','$nation', NOW())";
+
+    if ($conn->query($sql) === TRUE) {
+        header("Location: login.php"); // Redirige login.php
+        exit(); // Asegura que el script se detenga después de la redirección
+    }
+
+    $conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -12,13 +29,13 @@
     <div class="overlay"></div>
     <div class="container">
         <div class="cont-login">
-            <form action="" method="post" class="box-login">
+            <form action="signup.php" method="post" class="box-login">
                 <h1>Registrarse</h1>
 
                 <div class="form-control">
-                    <input type="text" class="input-login" name="username" id="username" placeholder="Nombre de Usuario">
-                    <input type="email" class="input-login" name="email" id="email" placeholder="Correo Electrónico">
-                    <input type="password" class="input-login" name="password" id="password" placeholder="Contraseña">
+                    <input type="text" class="input-login" name="username" id="username" placeholder="Nombre de Usuario" required>
+                    <input type="email" class="input-login" name="email" id="email" placeholder="Correo Electrónico" required>
+                    <input type="password" class="input-login" name="password" id="password" placeholder="Contraseña" required>
                     <select class="input-login" name="nationality" id="nationality">
                         <option value="">Seleccionar Nacionalidad</option>
                         <option value="pais1">País 1</option>
