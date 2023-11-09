ae15e7a4a3027a56d38fe84d024134597de27c46
imalegomez <imalegomez2004@gmail.com>
2023-11-09 03:54:09 -0300

    changes

--- conexiones/addLike.php
+++ conexiones/addLike.php
@@ -11,7 +11,7 @@ if (isset($_POST['id_publicacion'])) {
     $id_publicacion = $_POST['id_publicacion'];
     $id_usuario = $_SESSION['iduser']; // Asegúrate de obtener el ID del usuario de la sesión
 
-    // Realiza la inserción del "Me gusta" en la tabla t_likes
+    // Realiza la inserción o eliminación del "Me gusta" en la tabla t_likes
     require_once "cn.php"; // Asegúrate de que la conexión a la base de datos esté incluida
 
     // Verifica si el usuario ya ha dado "Me gusta" a esta publicación
@@ -32,10 +32,19 @@ if (isset($_POST['id_publicacion'])) {
             echo "Error al agregar 'Me gusta'.";
         }
     } else {
-        header("Location: ../pages/index.php"); // Redirige a la página de publicaciones
+        // El usuario ya ha dado "Me gusta", así que elimina el "Me gusta" de la tabla t_likes
+        $delete_query = $conn->prepare("DELETE FROM t_likes WHERE id_usuario = ? AND id_publicacion = ?");
+        $delete_query->bind_param('ii', $id_usuario, $id_publicacion);
+
+        if ($delete_query->execute()) {
+            header("Location: ../pages/index.php"); // Redirige a la página de publicaciones
+        } else {
+            echo "Error al eliminar 'Me gusta'.";
+        }
     }
 
     $conn->close();
 } else {
     echo "No se proporcionó una ID de publicación válida.";
 }
+?>
